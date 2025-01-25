22838ce7572016799b171c391d797ecd56bc2512
cpalomino1990 <89701110+cpalomino1990@users.noreply.github.com>
2025-01-24 22:48:18 -0500

    added public file

---
+++ src/tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+import defaultColors from "tailwindcss/colors";
+
+
+const { heroui } = require("@heroui/react");
+
+export default { // or 'class'
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        secondary: defaultColors.pink[800],
+        primary: defaultColors.pink[500],
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+
+} satisfies Config;
